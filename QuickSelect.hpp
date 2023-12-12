f46f0f226401072a8e5f249c36ee53067bdc9d00
Shelly Masih <53944084+shelly-masih@users.noreply.github.com>
2023-12-11 23:45:07 -0500

    Update QuickSelect.hpp

--- QuickSelect.hpp
+++ QuickSelect.hpp
@@ -99,9 +99,4 @@ private:
 
 
 
-// QuickSelect.hpp
-// int quickSelect ( std::vector<int>& nums, int& duration )
-// std::vector<int>::iterator hoarePartition ( std::vector<int>& nums, std::vector<int>::iterator low, std::vector<int>::iterator high )
-//    // hoarePartition precondition: low points to the first element in the subarray to be partitioned. The pivot is the last element in the subarray to be partitioned, and is pointed to by high.
-//    // hoarePartition returns an iterator to the pivot after it's placed.
-// Note that this implementation of hoarePartition makes it usable with different pivot selection methods, but also requires that you select your pivot and swap it into the last position prior to calling hoarePartition.
+
