f1b08bf903fc632a329943c722803d032af82ede
Shelly Masih <53944084+shelly-masih@users.noreply.github.com>
2023-12-11 23:30:26 -0500

    Update InPlaceMergeSort.hpp

--- InPlaceMergeSort.hpp
+++ InPlaceMergeSort.hpp
@@ -3,11 +3,47 @@
 
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include <chrono>
 
 class InPlaceMergeSort { 
     public:
-        int inPlaceMergeSort(std::vector<int>& nums, int& duration);
-        void inPlaceMergeSortHelper(std::vector<int>::iterator begin, std::vector<int>::iterator end, int &duration);
+        int inPlaceMergeSort(std::vector<int>& nums, int& duration){
+          auto begin = nums.begin();
+          auto end = nums.end();
+
+          inPlaceMergeSortHelper(begin, end, duration);
+
+          int size = nums.size();
+          int median;
+          if (size % 2 == 0) { // checks if vector is even
+            median = (nums[size / 2 - 1] + nums[size / 2]) / 2;
+          } else { // if vector is odd
+            median = nums[size / 2];
+          }
+
+          // Return a meaningful result (in this case, the median)
+          return median;
+        }
+
+        void inPlaceMergeSortHelper(std::vector<int>::iterator begin, std::vector<int>::iterator end, int &duration){
+          auto start = std::chrono::steady_clock::now();
+
+          if (std::distance(begin, end) <= 1) {
+            auto finish = std::chrono::steady_clock::now();
+            duration =
+                std::chrono::duration_cast<std::chrono::milliseconds>(finish - start)
+                    .count();
+            return;
+          }
+
+          auto midpoint = begin + std::distance(begin, end) / 2;
+
+          inPlaceMergeSortHelper(begin, midpoint, duration);
+          inPlaceMergeSortHelper(midpoint, end, duration);
+
+          std::inplace_merge(begin, midpoint, end);
+        }
 };
 
 #endif
