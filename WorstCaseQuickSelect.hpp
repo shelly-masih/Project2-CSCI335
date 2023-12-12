1f4b254cbb73644f2324520c512491f314066b0b
Shelly Masih <53944084+shelly-masih@users.noreply.github.com>
2023-12-11 23:32:00 -0500

    Update WorstCaseQuickSelect.hpp

--- WorstCaseQuickSelect.hpp
+++ WorstCaseQuickSelect.hpp
@@ -3,9 +3,14 @@
 
 #include <vector>
 
-  std::vector<int> worstCaseQuickSelect (void);
+  std::vector<int> worstCaseQuickSelect (void){const int size = 20000;
+                                              std::vector<int> worstCaseVector(size);
+                                              // Generate a sorted vector from 1 to 20000
+                                              for (int i = 1; i <= size; ++i) {
+                                                worstCaseVector[i - 1] = i;
+                                              }
+                                              // Shuffle the vector to make it harder for quickselect
+                                              std::random_shuffle(worstCaseVector.begin(), worstCaseVector.end());
+                                              return worstCaseVector;}
 
 #endif
-
-
-  
