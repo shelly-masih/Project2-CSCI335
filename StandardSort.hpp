88ed1d3785913b46420e60daa4df5025eac0728c
Shelly Masih <53944084+shelly-masih@users.noreply.github.com>
2023-12-11 23:31:33 -0500

    Update StandardSort.hpp

--- StandardSort.hpp
+++ StandardSort.hpp
@@ -6,6 +6,23 @@
 
 class StandardSort { 
     public:
-        int standardSort(std::vector<int>& nums, int& duration);
+        int standardSort(std::vector<int>& nums, int& duration){
+          auto start = std::chrono::steady_clock::now(); // start the clock
+          std::sort(nums.begin(), nums.end());           // sort the vector
+          int size = nums.size();                        // get size of vector
+          int median; // create variable to store the median
+
+          if (size % 2 == 0) { // checks if vector is even
+            median = std::min(nums[size / 2 - 1], nums[size / 2]);
+          } else { // if vector is odd
+            median = nums[size / 2];
+          }
+          auto end = std::chrono::steady_clock::now();
+          duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
+                         .count();
+
+          return median;
+        }
+
 }; 
 #endif
