c462d484a69ffaa0f6d5c77ea563de46213879e0
Shelly Masih <53944084+shelly-masih@users.noreply.github.com>
2023-12-11 23:35:55 -0500

    Update HalfSelectionSort.hpp

--- HalfSelectionSort.hpp
+++ HalfSelectionSort.hpp
@@ -1,13 +1,11 @@
 
+
 #ifndef HALFSELECTIONSORT_HPP
 #define HALFSELECTIONSORT_HPP
 
 #include <vector>
 #include <iostream>
 #include <chrono>
-
-class HalfSelectionSort { 
-    public:
 int halfSelectionSort(std::vector<int> &nums,
                                          int &duration) {
 
@@ -54,7 +52,3 @@ int halfSelectionSort(std::vector<int> &nums,
 
   return median;
 }
-}; 
-
-
-#endif
