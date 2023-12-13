c401dc27876341e0af9d43b1fecfa908489b4cd9
Shelly Masih <53944084+shelly-masih@users.noreply.github.com>
2023-12-12 23:41:25 -0500

    Update InPlaceMergeSort.hpp

--- InPlaceMergeSort.hpp
+++ InPlaceMergeSort.hpp
@@ -3,36 +3,41 @@
 #include <algorithm>
 #include <chrono>
 
-void inPlaceMergeSortHelper(std::vector<int>::iterator begin, std::vector<int>::iterator end, int &duration){
-  auto start = std::chrono::steady_clock::now();
+void inPlaceMergeSortHelper(std::vector<int>::iterator begin, std::vector<int>::iterator end, int &duration) {
+    auto start = std::chrono::steady_clock::now();
 
-  if (std::distance(begin, end) <= 1) {
-    auto finish = std::chrono::steady_clock::now();
-    duration =
-        std::chrono::duration_cast<std::chrono::milliseconds>(finish - start)
-            .count();
-    return;
-  }
+    if (std::distance(begin, end) <= 1) {
+        auto finish = std::chrono::steady_clock::now();
+        duration += std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count();
+        return;
+    }
 
-  auto midpoint = begin + std::distance(begin, end) / 2;
+    auto midpoint = begin + std::distance(begin, end) / 2;
 
-  inPlaceMergeSortHelper(begin, midpoint, duration);
-  inPlaceMergeSortHelper(midpoint, end, duration);
+    inPlaceMergeSortHelper(begin, midpoint, duration);
+    inPlaceMergeSortHelper(midpoint, end, duration);
 
-  std::inplace_merge(begin, midpoint, end);
+    std::inplace_merge(begin, midpoint, end);
 }
 
-int inPlaceMergeSort(std::vector<int>& nums, int& duration){
-  auto begin = nums.begin();
-  auto end = nums.end();
+int inPlaceMergeSort(std::vector<int>& nums, int& duration) {
+    auto begin = nums.begin();
+    auto end = nums.end();
 
-  inPlaceMergeSortHelper(begin, end, duration);
+    auto start = std::chrono::steady_clock::now();
+    inPlaceMergeSortHelper(begin, end, duration);
+    auto finish = std::chrono::steady_clock::now();
 
-  int size = nums.size() / 2;
-  int median;
-  if (size % 2 == 0) { // checks if vector is even
-    return (nums[size - 1] + nums[size]) / 2;
-  } else { // if vector is odd
-    return nums[size];
-  }
+    duration += std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count();
+
+    int size = nums.size();
+    int median;
+
+    if (size % 2 == 0) { // checks if vector is even
+        median = (nums[size / 2 - 1] + nums[size / 2]) / 2;
+    } else { // if vector is odd
+        median = nums[size / 2];
+    }
+
+    return median;
 }
