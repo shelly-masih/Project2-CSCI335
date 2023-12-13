694f7e41f1de11980de4700e8f1305ad0d120666
Shelly Masih <53944084+shelly-masih@users.noreply.github.com>
2023-12-12 23:52:13 -0500

    Update InPlaceMergeSort.hpp

--- InPlaceMergeSort.hpp
+++ InPlaceMergeSort.hpp
@@ -1,43 +1,27 @@
 #include <vector>
-#include <iostream>
-#include <algorithm>
 #include <chrono>
+#include <algorithm>
 
-void inPlaceMergeSortHelper(std::vector<int>::iterator begin, std::vector<int>::iterator end, int &duration) {
-    auto start = std::chrono::steady_clock::now();
+void inPlaceMergeSortHelper(std::vector<int>& nums, int begin, int end) {
+    if (begin >= end) return;
 
-    if (std::distance(begin, end) <= 1) {
-        auto finish = std::chrono::steady_clock::now();
-        duration += std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count();
-        return;
-    }
+    int mid = begin + (end - begin) / 2;
+    inPlaceMergeSortHelper(nums, begin, mid);
+    inPlaceMergeSortHelper(nums, mid + 1, end);
 
-    auto midpoint = begin + std::distance(begin, end) / 2;
-
-    inPlaceMergeSortHelper(begin, midpoint, duration);
-    inPlaceMergeSortHelper(midpoint, end, duration);
-
-    std::inplace_merge(begin, midpoint, end);
+    std::inplace_merge(nums.begin() + begin, nums.begin() + mid + 1, nums.begin() + end + 1);
 }
 
 int inPlaceMergeSort(std::vector<int>& nums, int& duration) {
-    auto begin = nums.begin();
-    auto end = nums.end();
-
     auto start = std::chrono::steady_clock::now();
-    inPlaceMergeSortHelper(begin, end, duration);
+    inPlaceMergeSortHelper(nums, 0, nums.size() - 1);
     auto finish = std::chrono::steady_clock::now();
-
     duration += std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count();
 
-    int size = nums.size();
-    int median;
-
-    if (size % 2 == 0) { // checks if vector is even
-        median = (nums[size / 2 - 1] + nums[size / 2]) / 2;
-    } else { // if vector is odd
-        median = nums[size / 2];
+    int mid = nums.size() / 2;
+    if (nums.size() % 2 == 0) {
+        return nums[mid - 1];
+    } else {
+        return nums[mid];
     }
-
-    return median;
 }
