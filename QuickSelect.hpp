9008568d60d336c5798175e65354731774eb9877
Shelly Masih <53944084+shelly-masih@users.noreply.github.com>
2023-12-15 21:49:14 -0500

    Update QuickSelect.hpp

--- QuickSelect.hpp
+++ QuickSelect.hpp
@@ -1,39 +1,91 @@
-
+#include <algorithm>
+#include <chrono>
 #include <iostream>
 #include <vector>
 
-std::vector<int>::iterator hoarePartition(std::vector<int>& nums, std::vector<int>::iterator low, std::vector<int>::iterator high) {
-    int pivot = *(high - 1);
-    std::vector<int>::iterator i = low - 1;
+std::vector<int>::iterator hoarePartition(std::vector<int> &nums,
+                                          std::vector<int>::iterator low,
+                                          std::vector<int>::iterator high) {
+  int pivot = *low;
+  auto i = low;
+  auto j = high;
 
-    for (std::vector<int>::iterator j = low; j < high - 1; ++j) {
-        if (*j <= pivot) {
-            ++i;
-            std::swap(*i, *j);
-        }
+  while (true) {
+    do {
+      ++i;
+    } while (*i < pivot);
+
+    do {
+      --j;
+    } while (*j > pivot);
+
+    if (i >= j) {
+      return j;
     }
 
-    std::swap(*(i + 1), *(high - 1));
-    return i + 1;
+    std::iter_swap(i, j);
+  }
 }
 
-int quickSelect(std::vector<int>& nums, int k) {
-    int low = 0;
-    int high = nums.size();
+void medianOfThree(std::vector<int> &nums, std::vector<int>::iterator low,
+                   std::vector<int>::iterator high) {
+  auto mid = low + (high - low) / 2;
 
-    while (low < high) {
-        std::vector<int>::iterator partition = hoarePartition(nums, nums.begin() + low, nums.begin() + high);
-        int pivotIndex = std::distance(nums.begin(), partition);
+  if (*low > *mid)
+    std::iter_swap(low, mid);
+  if (*low > *high)
+    std::iter_swap(low, high);
+  if (*mid > *high)
+    std::iter_swap(mid, high);
+}
 
-        if (pivotIndex == k) {
-            return *partition;
-        } else if (pivotIndex < k) {
-            low = pivotIndex + 1;
-        } else {
-            high = pivotIndex;
-        }
+void QuickSelectHelper(std::vector<int> &nums,
+  std::vector<int>::iterator lower,
+  std::vector<int>::iterator upper,
+  std::vector<int>::iterator middle) {
+  
+}
+
+int quickSelect(std::vector<int> &nums, int &duration) {
+  auto start = std::chrono::high_resolution_clock::now();
+
+  int k;
+
+  if (nums.size() % 2 == 0) {
+    k = (nums.size() / 2) - 1;
+  } else {
+    k = nums.size() / 2;
+  }
+
+  std::vector<int>::iterator low = nums.begin();
+  std::vector<int>::iterator high = nums.end() - 1;
+  std::vector<int>::iterator mid = std::next(nums.begin(), k);
+
+  medianOfThree(nums, low, high);
+
+  // Ensure the initial pivot is in the correct place
+  std::vector<int>::iterator pivotIndex = hoarePartition(nums, low, high);
+
+  while (low < high) {
+    if (k < pivotIndex - nums.begin()) {
+      high = pivotIndex;
+    } else if (k > pivotIndex - nums.begin()) {
+      low = pivotIndex + 1;
+    } else {
+      duration = std::chrono::duration_cast<std::chrono::microseconds>(
+                     std::chrono::high_resolution_clock::now() - start)
+                     .count();
+      return *pivotIndex;
     }
 
-    // If k is out of bounds, return an appropriate value (e.g., -1).
-    return -1;
+    medianOfThree(nums, low, high);
+    pivotIndex = hoarePartition(nums, low, high);
+  }
+
+  duration = std::chrono::duration_cast<std::chrono::microseconds>(
+                 std::chrono::high_resolution_clock::now() - start)
+                 .count();
+
+  return *low;
 }
+
