e425611ad0a6b0a20d1e4148484963878eb556f4
Shelly Masih <53944084+shelly-masih@users.noreply.github.com>
2023-12-16 20:45:42 -0500

    Update HalfHeapSort.hpp

--- HalfHeapSort.hpp
+++ HalfHeapSort.hpp
@@ -4,20 +4,20 @@
 #include <vector>
 
 inline int leftChild(int i) { return 2 * i; }
-void percDown(std::vector<int> &heap, std::vector<int>::size_type hole) {
+void percDown(std::vector<int> &heap, std::vector<int>::size_type hole) { // percolate down function
   do {
     int child = leftChild(hole);
 
-    // Check if there is a right child and if it's smaller than the left child
+    // Checks if there is a right child and if it's smaller than the left child
     if (child + 1 < heap.size() && heap[child + 1] < heap[child]) {
-      ++child; // Use the right child instead
+      ++child; // increment
     }
 
     // If the child is smaller than heap[0], move the child up
     if (heap[child] < heap[0]) {
       heap[hole] = std::move(heap[child]);
     } else {
-      break; // Found the right position for heap[0]
+      break; // finds the right position for heap[0]
     }
 
     hole = child; // Update hole to the child's index for the next iteration
@@ -28,10 +28,13 @@ void percDown(std::vector<int> &heap, std::vector<int>::size_type hole) {
 }
 
 void buildHeap(std::vector<int> &heap) {
-  for (int parentNodeIdx = (heap.size() - 1) / 2; parentNodeIdx > 0;
-       --parentNodeIdx) {
-    heap[0] = heap[parentNodeIdx];
-    percDown(heap, parentNodeIdx);
+  // Start from the last non-leaf node and move up the tree
+  for (int parentNode = (heap.size() - 1) / 2; parentNode > 0;
+       --parentNode) {
+    // Move the current parent node value to the root (index 0)
+    heap[0] = heap[parentNode];
+    // Restore the heap property by calling percDown
+    percDown(heap, parentNode);
   }
 }
 
@@ -39,10 +42,14 @@ int halfHeapSort(std::vector<int> &nums, int &duration) {
   auto start = std::chrono::steady_clock::now();
 
   nums.push_back(nums[0]);
+  // build a max heap
   buildHeap(nums);
 
+  // Calculate the size of the heap
   int size = (nums.size() - 2) / 2;
   int i = 0;
+
+  // Iterate through the heap
   while (i < size) {
     std::swap(nums[0], nums[nums.size() - 1]);
     nums.pop_back();
@@ -52,5 +59,6 @@ int halfHeapSort(std::vector<int> &nums, int &duration) {
   auto end = std::chrono::steady_clock::now();
   duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
                  .count();
+  // Return the minimum value in the sorted array
   return nums[1];
 }
