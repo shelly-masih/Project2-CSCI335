26cfb5b7a56786b412931cfdf448c350b8178874
Shelly Masih <53944084+shelly-masih@users.noreply.github.com>
2023-12-16 20:34:50 -0500

    Update HalfHeapSort.hpp

--- HalfHeapSort.hpp
+++ HalfHeapSort.hpp
@@ -1,59 +1,56 @@
-#include <vector>
 #include <algorithm>
 #include <chrono>
+#include <iostream>
+#include <vector>
 
-inline std::vector<int>::size_type leftChild(std::vector<int>::size_type i) {
-    return 2 * i + 1;
-}
-
+inline int leftChild(int i) { return 2 * i; }
 void percDown(std::vector<int> &heap, std::vector<int>::size_type hole) {
-    std::vector<int>::size_type child;
-    int tmp = std::move(heap[hole]);
+  do {
+    int child = leftChild(hole);
 
-    for (; leftChild(hole) < heap.size(); hole = child) {
-        child = leftChild(hole);
-        if (child != heap.size() - 1 && heap[child] < heap[child + 1]) {
-            ++child;
-        }
-        if (tmp < heap[child]) {
-            heap[hole] = std::move(heap[child]);
-        } else {
-            break;
-        }
+    // Check if there is a right child and if it's smaller than the left child
+    if (child + 1 < heap.size() && heap[child + 1] < heap[child]) {
+      ++child; // Use the right child instead
     }
-    heap[hole] = std::move(tmp);
+
+    // If the child is smaller than heap[0], move the child up
+    if (heap[child] < heap[0]) {
+      heap[hole] = std::move(heap[child]);
+    } else {
+      break; // Found the right position for heap[0]
+    }
+
+    hole = child; // Update hole to the child's index for the next iteration
+
+  } while (hole * 2 < heap.size());
+
+  heap[hole] = std::move(heap[0]); // Move heap[0] down to the child position
 }
 
 void buildHeap(std::vector<int> &heap) {
-    // Start from the last non-leaf node and perform percDown
-    for (std::vector<int>::size_type i = heap.size() / 2; i != static_cast<std::vector<int>::size_type>(-1); --i) {
-        percDown(heap, i);
-    }
+  for (int parentNodeIdx = (heap.size() - 1) / 2; parentNodeIdx > 0;
+       --parentNodeIdx) {
+    heap[0] = heap[parentNodeIdx];
+    percDown(heap, parentNodeIdx);
+  }
 }
 
 int halfHeapSort(std::vector<int> &nums, int &duration) {
-    auto start = std::chrono::steady_clock::now();
+  auto start = std::chrono::steady_clock::now();
 
-    // Build heap
-    buildHeap(nums);
+  nums.push_back(nums[0]);
+  buildHeap(nums);
 
-    // Remove n/2 elements from the heap
-    for (std::vector<int>::size_type j = 0; j < nums.size() / 2; ++j) {
-        // Move the first element to the end of the vector
-        std::swap(nums[0], nums[nums.size() - 1 - j]);
-
-        // Perform percDown to restore the heap property
-        percDown(nums, 0);
-    }
-
-    // Median is now at the root of the remaining heap
-    int median = nums[0];
-
-    // Resize the vector
-    nums.resize(nums.size() / 2);
-
-    auto end = std::chrono::steady_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-
-    return median;
+  int size = (nums.size() - 2) / 2;
+  int i = 0;
+  while (i < size) {
+    std::swap(nums[0], nums[nums.size() - 1]);
+    nums.pop_back();
+    percDown(nums, 1);
+    ++i;
+  }
+  auto end = std::chrono::steady_clock::now();
+  duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
+                 .count();
+  return nums[1];
 }
